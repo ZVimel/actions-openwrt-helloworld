ab4269c2984adf37f33dd613b037cad31beccfd9
Vimel <Vimel@iov.me>
2022-11-27 11:53:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 rm -rf package/lean/luci-theme-argon
@@ -21,7 +21,3 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # rm -rf package/lean/OpenClash
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
-
-
-
-
