3e4f6c7a37a60d3cb39a3d544878ab51c9bd5d5f
Vimel <Vimel@iov.me>
2022-11-27 11:45:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 # rm -rf feeds/packages/lang &&  svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang
 # curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-mipsle-softfloat.tar.gz -o /tmp/clash.tar.gz
@@ -18,4 +18,3 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Ma
 # mkdir -p ./package/luci-app-openclash/files/etc/openclash/core
 # mv /tmp/clash ./package/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
 # rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-
